cc66625a68e3007bd06cc1d93bafe97d41c3ca4d
Yousef-Ashraff <yousefashraf970@gmail.com>
2024-12-24 22:02:19 +0200

    Update database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9511490da7ea7ff9fbca1bf25a61a9b5926b639ebfa0c11b88833b290418c178
-size 721
+import pickle
+# Function to load the database encoding from a pickle file
+def load_database_encoding(file_path):
+    with open(file_path, 'rb') as f:
+        return pickle.load(f)
+
+# Function to load the Account database from a pickle file
+def load_account_db(file_path):
+    with open(file_path, 'rb') as f:
+        return pickle.load(f)
+
+# Function to save the Account database to a pickle file
+def save_account_db(file_path, account_db):
+    with open(file_path, 'wb') as f:
+        pickle.dump(account_db, f)
+
+# Function to save the database encoding to a pickle file
+def save_database_encoding(file_path, encoding_db):
+    with open(file_path, 'wb') as f:
+        pickle.dump(encoding_db, f)
+
+
