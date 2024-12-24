2e037d0dcb809ca91516eda7a49799d4c39a0f12
Yousef-Ashraff <yousefashraf970@gmail.com>
2024-12-24 22:01:09 +0200

    Update Init_acc_db.py

--- Init_acc_db.py
+++ Init_acc_db.py
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aa0015d35e1c93b2463d6cf140ffbae94a780042fc03a9e5058daa9275daf490
-size 306
+from database_utils import  load_database_encoding, save_account_db
+from Account_Class import Account
+
+loaded_database_enc = load_database_encoding('database_encoding.pkl')
+Account_db = [Account(label, '', 0) for label in loaded_database_enc.keys()]
+save_account_db('account_db.pkl', Account_db)
+
+
