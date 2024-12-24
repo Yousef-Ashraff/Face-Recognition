f3796963e54139023105a3626dd1c18c72e2a757
Yousef-Ashraff <yousefashraf970@gmail.com>
2024-12-24 22:02:39 +0200

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b0c6eddfbee26b69d11dac9b3612de864a6b4e234cf80b726fe407ddd38f9145
-size 566
+from database_utils import load_account_db, load_database_encoding,save_database_encoding, save_account_db
+from Account_Class import Account
+Account_db= load_account_db('account_db.pkl')
+# print(Account_db[0].names)  # Check data before saving
+
+# # Account_db.append(Account('mohamed', '', 0))
+# # print(len(Account_db))
+# # print(Account_db[0].cnt)
+# print(Account.NumberOfNameInAcc)
+
+# save_account_db('account_db.pkl', Account_db)
+loaded_database_enc= load_database_encoding('database_encoding.pkl')
+print(len(loaded_database_enc['Dwayne Johnson']))
