5ff9d9e2a2e19e3cf47ea0b616e71f9b8452eb6a
Yousef-Ashraff <yousefashraf970@gmail.com>
2024-12-24 22:02:02 +0200

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,3 +1,47 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d8d32b42396b6b5d74235898b4b352a8375b0ccaab8040d4df827377ab3be10f
-size 1677
+from preprocessing import img_to_encoding
+import numpy as np
+
+
+def who_is_it(image_path, database, model):
+    """
+    Implements face recognition for the office by finding who is the person on the image_path image.
+    
+    Arguments:
+        image_path -- path to an image
+        database -- database containing image encodings along with the name of the person on the image
+        model -- your Inception model instance in Keras
+    
+    Returns:
+        min_dist -- the minimum distance between image_path encoding and the encodings from the database
+        identity -- string, the name prediction for the person on image_path
+    """
+    
+    ### START CODE HERE
+
+    ## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above. ## (≈ 1 line)
+    encoding = img_to_encoding(image_path, model)
+    
+    ## Step 2: Find the closest encoding ##
+    
+    # Initialize "min_dist" to a large value, say 100 (≈1 line)
+    min_dist = 100
+    
+    # Loop over the database dictionary's names and encodings.
+    for (name, encs) in database.items():
+        
+        # Compute L2 distance between the target "encoding" and the current db_enc from the database. (≈ 1 line)
+        for db_enc in encs:
+            dist = np.linalg.norm(db_enc - encoding, ord = 2)
+
+            # If this distance is less than the min_dist, then set min_dist to dist, and identity to name. (≈ 3 lines)
+            if dist < min_dist:
+                min_dist = dist
+                identity = name
+    ### END CODE HERE
+    if min_dist > 0.9:
+        return ""
+    else:
+        return identity
+
+        
+    
